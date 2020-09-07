f54229225bc3c0e41651919fce4e1735872dc755
Echono <wonger1324@gmail.com>
2020-09-07 11:46:41 +0200

    Addition of person

--- TestWorldAgain/TestWorldAgain/Program.cs
+++ TestWorldAgain/TestWorldAgain/Program.cs
@@ -1,13 +1,21 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Dynamic;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace TestWorldAgain {
     class Program {
+
+        static List<Person> people { get; set; }
+
         static void Main(string[] args) {
-            Console.WriteLine("Hej");
+            people = new List<Person>();
+            foreach(Person p in people) {
+                Console.WriteLine(p.name + " " + p.age);
+            }
         }
     }
 }
