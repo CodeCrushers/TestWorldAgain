092a04f21b967badd0805d2c7a7071b281bf7e66
Echono <wonger1324@gmail.com>
2020-09-07 12:16:37 +0200

    Added Person

--- TestWorldAgain/TestWorldAgain/Model/Person.cs
+++ TestWorldAgain/TestWorldAgain/Model/Person.cs
@@ -6,5 +6,14 @@ using System.Threading.Tasks;
 
 namespace TestWorldAgain.Model {
     class Person {
+
+        public string name { get; set; }
+        public int age { get; set; }
+
+        public Person(string name, int age) {
+            this.name = name;
+            this.age = age;
+        }
+
     }
 }
